1bc8238fd9064712f1317c388effc68fcaf63270
DevAtDawn <86164137+DevAtDawn@users.noreply.github.com>
2022-09-24 01:56:12 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 setup(
-    name='openai_cli',
+    name='openai_cli_test',
     version='0.0.1',
-    packages=['openai_cli'],
+    scripts=['bash_scripts/openai_auto.sh'],
+    packages=['openai_cli_test'],
     entry_points={
         'console_scripts': [
-            'openai_cli=openai_cli.openai_cli:cli'
+            'openai_cli_test=openai_cli_test.openai_cli_test:cli'
         ]
     }
 )
